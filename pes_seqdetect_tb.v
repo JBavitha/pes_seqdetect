ed5956fe82051181351c370e5afa4cadbc1e4aab
JBavitha <142578450+JBavitha@users.noreply.github.com>
2023-10-19 19:39:50 +0530

    Update pes_seqdetect_tb.v

--- pes_seqdetect_tb.v
+++ pes_seqdetect_tb.v
@@ -1,47 +1,31 @@
-`timescale 1ns / 1ps
-// fpga4student.com: FPGA projects, Verilog projects, VHDL projects
-// Verilog project: Verilog code for Sequence Detector using Moore FSM
-// Verilog Testbench for Sequence Detector using Moore FSM 
 module pes_seqdetect_tb;
+reg sequence_in,clock,reset;
+wire detector_out;
+iiitb_SDM m1(sequence_in,clock,reset,detector_out);
+initial
+begin
+$dumpfile("sqnsdet_tb.vcd");
+$dumpvars(0);
+clock=0;sequence_in=0;
+reset=1;
+#10 reset=1;
+#10 reset =0;
+$monitor($time, , ,"c=%b",clock,,"y=%b",detector_out,,"r=%b",reset,,"d=%b",sequence_in);
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=0;
 
- // Inputs
- reg sequence_in;
- reg clock;
- reg reset;
-
- // Outputs
- wire detector_out;
-
- // Instantiate the Sequence Detector using Moore FSM
-pes_seqdetect uut (
-  .sequence_in(sequence_in), 
-  .clock(clock), 
-  .reset(reset), 
-  .detector_out(detector_out)
- );
- initial begin
- clock = 0;
-  $dumpfile("sd_fsm.vcd");
- $dumpvars(0);
- forever #5 clock = ~clock;
- end 
- initial #200 $finish;  
- initial begin
-  // Initialize Inputs
-  sequence_in = 0;
-  reset = 1;
-
-  #10;reset = 0;
-  
-  #10;sequence_in = 1;
-  #10; sequence_in = 0;
-  #10; sequence_in = 1; 
-  #10;sequence_in = 1; 
-  #10;sequence_in = 1; 
-  #10;sequence_in =0;
- 
-  // Add stimulus here
-
- end
-
+end
+always
+#5 clock=~clock;
+initial
+#200 $finish ;
 endmodule
